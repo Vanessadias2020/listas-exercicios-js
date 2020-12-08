f1d99f243c312d687c5915a99a587de22b065141
Vanessadias2020 <cat.vanessa@hotmail.com>
2020-12-07 22:18:03 -0300

    resolução de exercicio

---
+++ js/funcoes.js
@@ -0,0 +1,7 @@
+ console.log("Vanessa Dias");
+ let nome = prompt("Informe seu nome");
+ alert(" Olá " + " seja bem vindo! ");
+ let numero1 = prompt("Digite um número");
+ let numero2 = prompt("Digite outro número");
+
+ alert(numero1 * numero2);
